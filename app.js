ed450aca2c2a2978ed409b08ffb9d3124ba8fa62
Mattias Petter Johansson <mpj@mpj.me>
2012-06-16 16:43:58 +0200

    Clean template

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
+  
+  app.set('view options', {
+    layout: false
+  });
+  
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
